5303511f138d2e128af12dbbc668be102817f79b
Eorgregix <gregoirevscloud@hotmail.fr>
2018-04-21 22:46:09 +0200

    shit

--- source/Knife.hx
+++ source/Knife.hx
@@ -5,13 +5,11 @@ import flixel.FlxObject;
 import flixel.FlxSprite;
 import flixel.math.FlxPoint;
 
-class Peeler extends FlxSprite 
+class Knife extends FlxSprite 
 {
 	public var spriteResolution: Int = 32;
-	
 	public var maxHealth:Float = 100;
 	public var currentHealt:Float = 100;
-	
 	public var speed:Float = 200;
 	public var sliceDmg:Float = 10;
 	public var dmg: Float = 10;
@@ -24,11 +22,11 @@ class Peeler extends FlxSprite
 		loadGraphic(AssetPaths.knife__1__0__png, true, spriteResolution, spriteResolution);
 		setFacingFlip(FlxObject.RIGHT, false, false);
 		setFacingFlip(FlxObject.LEFT, true, false);
-		//reset(x , y +spriteResolution / 3);
-		//setSize(spriteResolution - 5, 10);
+		//setSize(spriteResolution-5, 10);
+		//centerOffsets();
 		//offset.set(0, spriteResolution/3);
 		//scale.set(0.5, 0.5);
-		//updateHitbox();
+		updateHitbox();
 		//animation.add("lr", [3, 4, 3, 5], 6, false);
 		//animation.add("u", [6, 7, 6, 8], 6, false);
 		//animation.add("d", [0, 1, 0, 2], 6, false);

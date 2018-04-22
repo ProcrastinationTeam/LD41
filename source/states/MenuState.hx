eb05e43795402e5a3be9ffd0134ff93af0a98b5c
Guillaume Ambrois <guillaume.ambrois@octopeek.com>
2018-04-22 19:15:20 +0200

    Menu temp

--- source/states/MenuState.hx
+++ source/states/MenuState.hx
@@ -1,14 +1,38 @@
 package states;
 
+import flixel.FlxSprite;
 import flixel.FlxState;
 import flixel.FlxG;
+import flixel.text.FlxText;
+import flixel.tweens.FlxEase;
+import flixel.tweens.FlxTween;
+import flixel.util.FlxAxes;
 
 class MenuState extends FlxState
 {
+	private var credits : Array<String> = ["Made in 72h during the 41th Ludum Dare", "Art - Thomas Fantino @fantifanta", "Musics and sounds - "];
+	
 	override public function create():Void
 	{
 		super.create();
 		trace('menu');
+		
+		var titleSprite = new FlxSprite();
+		titleSprite.loadGraphic(AssetPaths.assiette__png);
+		titleSprite.screenCenter(FlxAxes.XY);
+		
+		add(titleSprite);
+		
+		var pressToPlay = new FlxText();
+		pressToPlay.text = "Press space to play!";
+		pressToPlay.screenCenter(FlxAxes.XY);
+		add(pressToPlay);
+		
+		FlxTween.tween(pressToPlay, {alpha: 0}, 0.7, {type: FlxTween.PINGPONG, ease: FlxEase.smoothStepInOut});
+		
+		var credits = new FlxText();
+		credits.text = "Made in 72h for the 41th Ludum Dare";
+		credits.screenCenter(FlxAxes.XY);
 	}
 
 	override public function update(elapsed:Float):Void
